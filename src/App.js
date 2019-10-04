b29773b9bfa3b715d07ff2aeb675e162423d2c79
Marcus Almeida <s030827@gmail.com>
2019-10-04 16:18:52 -0300

    Login Componet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
+import logo from './assets/logo.svg';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello Word!</h1>
-      </header>
+    <div className="container">
+      <img src={logo} alt="AirCnC - Marcus Almeida"/>
+
+      <div className="content">
+        <p>
+          Ofereça <strong>Spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa.
+        </p>
+
+        <form>
+          <label htmlFor="email">E-mail *</label>
+          <input 
+            type        = "email"
+            id          = "email"
+            placeholder = "Seu melhor E-mail"
+          />
+
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
