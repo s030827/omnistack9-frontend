c69140fd2ed62e46209a21c2b4297f449e62495a
Marcus Almeida <s030827@gmail.com>
2019-10-06 15:51:55 -0300

    Get response from dashboard filtering user

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Api from '../../services/api';
 
 export default function Dashboard() {
-  return <div />
+  useEffect(() => {
+    async function loadSpots() {
+      const user_id  = localStorage.getItem('user');
+      const response = await Api.get('/dashboard', 
+        { headers: { user_id }}
+      );
+
+      console.log(response.data);
+    };
+
+    loadSpots();
+
+  }, []);
+
+  return <h1>Dashboard</h1>
 };
