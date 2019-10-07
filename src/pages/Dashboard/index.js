92f03931f2fe1948d614b5965e6c79c120804c2e
Marcus Almeida <s030827@gmail.com>
2019-10-07 00:39:13 -0300

    Apply css on dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Api from '../../services/api';
+import './styles.css';
 
 export default function Dashboard() {
   const [spots, setSpots] = useState([]);
@@ -24,9 +25,9 @@ export default function Dashboard() {
       <ul className="spot-list">
         {spots.map(spot => (
           <li key={spot._id}>
-            <header />
+            <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }} />
             <strong>{spot.company}</strong>
-            <span>{spot.price}</span>
+            <span>{spot.price ? `R$ ${spot.price}/Dia` : 'Gratuito'}</span>
           </li>
         ))}
       </ul>
